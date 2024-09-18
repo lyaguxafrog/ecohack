9266b5abebee63ba78665795ae53e6020b5f1b25
Kostya <147755909+Xabenskie@users.noreply.github.com>
2024-09-18 14:38:28 +0300

    [+] Auth form (#4)

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import LoginForm from '@/components/login/login-form';
+import LoginForm from '@/components/auth/login/login-form';
 
 export default function LoginPage() {
   return <LoginForm />;
---
+++ client/components/auth/register/register-form.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { Button } from '@nextui-org/button';
+import { DatePicker } from '@nextui-org/date-picker';
+import { Input } from '@nextui-org/input';
+import { Link } from '@nextui-org/link';
+
+export default function RegisterForm() {
+  return (
+    <div className="w-[60%] px-32">
+      <h1 className="font-bold text-2xl mb-5">Регистрация</h1>
+      <div className="flex flex-col justify-center items-center self-center gap-y-5">
+        <div className="flex gap-2">
+          <Input label="Имя" placeholder="Иван" size="sm" />
+          <Input label="Фамилия" placeholder="Иванов" size="sm" />
+        </div>
+        <Input label="Номер телефона" placeholder="+79991112233" size="sm" />
+        <Input label="Email" placeholder="JohnDoe@mail.ru" size="sm" />
+        <div className="w-full max-w-xl flex flex-row gap-4">
+          <DatePicker showMonthAndYearPickers label="День Рождения" />
+        </div>
+        <div className="flex gap-x-2 items-center ml-auto -mt-3">
+          <p className="text-sm text-gray-400">Есть аккаунт? </p>
+          <Link className="text-sm" href="login" underline="hover">
+            Войти
+          </Link>
+        </div>
+
+        <Button color="primary" size="lg">
+          Зарегистрироваться
+        </Button>
+      </div>
+    </div>
+  );
+}
