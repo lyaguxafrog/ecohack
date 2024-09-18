6ef3dde7a0c91fb420aecf9b6e39a9ea96f3d7b0
Makridenko Adrian <adrian.makridenko@proton.me>
2024-09-19 02:03:54 +0300

    tls

--- ecopass.sh
+++ ecopass.sh
@@ -6,11 +6,20 @@ config_app() {
     cp kernel/config/local_settings.example kernel/config/local_settings.py
 }
 
+get_tls() {
+    wget https://acme-v02.api.letsencrypt.org/directory
+    mv directory letsencrypt
+    exit 0
+}
+
 
 
 case "$1" in
     config)
         config_app
+        ;;
+    ssl)
+        get_tls)
         ;;
     *)
     exit 0
