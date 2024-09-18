4aa2dd1de22ee79431397df546a8e24c54e469b6
ShIIIrochka <shiroooooo@yandex.ru>
2024-09-18 17:55:25 +0300

    добавила новое приложение в settings

---
+++ kernel/reviews/models/reviews_models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from events.models import Events
+
+
+class Reviews(models.Model):
+    """
+    *title: Заголовок отзыва
+    *description: Описание отзыва
+    *event: событие, к которому прикреплен отзыв
+    """
+
+    description = models.TextField(
+        verbose_name=_("Описание отзыва"),
+        max_length=1024
+    )
+    rate = models.FloatField(
+        verbose_name=_("Оценка события"),
+        default=5.0
+    )
+    event = models.ForeignKey(
+        to=Events,
+        on_delete=models.CASCADE
+    )
+    # TODO: author
