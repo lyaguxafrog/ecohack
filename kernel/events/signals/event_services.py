3efa866ad6e50d213344deff3c36cf8ebf6ce403
ShIIIrochka <shiroooooo@yandex.ru>
2024-09-18 13:55:18 +0300

    сервис удаления события, добавила докстринги и return

--- kernel/events/signals/event_services.py
+++ kernel/events/signals/event_services.py
@@ -2,17 +2,45 @@
 
 from typing import Optional
 
+from django.db.transaction import atomic
+
 from events.models import Events
 
 
+@atomic
 def create_event(
     title: str,
     description: Optional[str] = None
 ) -> Events:
+    """
+    Сервис для создания события
+
+    :param title: Заголовок события
+    :param description: Описание события
+    """
+
     try:
-        Events.objects.create(
+        event = Events.objects.create(
             title=title,
             description=description
         )
     except Exception as err:
         raise Exception(err)
+
+    return event
+
+@atomic
+def delete_event(
+    event: Events
+) -> bool:
+    """
+    Сервис для удаления события
+
+    :param event: Объект события
+    """
+
+    try:
+        event.objects.delete()
+    except Exception as err:
+        raise Exception(err)
+    return True
