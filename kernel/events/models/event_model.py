90738bb783e6a500e857440ce28219d123b4ca61
Makridenko Adrian <adrian.makridenko@proton.me>
2024-09-19 04:16:50 +0300

    fix bugs

--- kernel/events/models/event_model.py
+++ kernel/events/models/event_model.py
@@ -13,10 +13,12 @@ class Events(models.Model):
     * description: Описание события
     * rating: Рейтинг события
     """
+
     title = models.CharField(
         verbose_name=_("Заголовок"),
         max_length=256
     )
+
     description = models.CharField(
         verbose_name=_("Описание"),
         max_length=1024,
@@ -26,29 +28,37 @@ class Events(models.Model):
         verbose_name=_("Рейтинг"),
         null=True
     )
+
     date = models.DateTimeField(
         verbose_name=_(""),
         auto_now=True
     )
+
     longtitude = models.CharField(
         verbose_name=_("Долгота"),
         null=True
     )
+
     latitude = models.CharField(
         verbose_name=_("Широта"),
         null=True
     )
+
     is_archive = models.BooleanField(
         verbose_name=_("архивно ли событие"),
         default=False
     )
+
     organizer = models.ForeignKey(
         verbose_name='Организатор события',
-        to=User
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="event_organaser",
     )
+
     guests = models.ManyToManyField(
         verbose_name='Гости на событии',
         to=User,
-        null=True,
-        blank=True
+        null=True, blank=True,
+        related_name="event_guess"
     )
