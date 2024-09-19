728806d4f81950ab22466de6751276568aa5918f
Xabenskie <lukyanovkr@gmail.com>
2024-09-19 13:35:33 +0300

    [+] log reg

--- client/components/auth/login/login-form.tsx
+++ client/components/auth/login/login-form.tsx
@@ -1,25 +1,81 @@
 'use client';
 import { Button } from '@nextui-org/button';
+import { Card, CardBody, CardHeader } from '@nextui-org/card';
 import { Input } from '@nextui-org/input';
 import { Link } from '@nextui-org/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { checkAuth, signIn } from '@/store/api-actions';
+import { useAppDispatch, useAppSelector } from '@/types';
+import { ICheckAuth } from '@/types/auth-types';
 
 export default function LoginForm() {
+  const { register, handleSubmit } = useForm<ICheckAuth>();
+  const dispatch = useAppDispatch();
+  const navigation = useRouter();
+  const [shownPhoneToCall, setShownPhoneToCall] = useState(false);
+  const [savedPhone, setSavedPhone] = useState<string>('');
+  const phoneToCall = useAppSelector((state) => state.DATA_STATE.phoneToCall);
+
+  const onSubmit = (data: ICheckAuth) => {
+    dispatch(signIn({ signInInput: data })).then((reqData) => {
+      if (reqData.meta.requestStatus === 'fulfilled') {
+        setShownPhoneToCall(true);
+        console.log(phoneToCall);
+        setSavedPhone(data.phone);
+      }
+    });
+  };
+
+  const dataD = {
+    phone: savedPhone,
+  };
+
+  const showPhoneToCallHandler = () => {
+    dispatch(checkAuth({ registerUserInput: dataD })).then((reqData) => {
+      if (reqData.meta.requestStatus === 'fulfilled') {
+        navigation.push('/');
+      } else {
+        // TODO: show error message
+        console.log(reqData);
+      }
+    });
+  };
+
   return (
-    <div className="w-[60%] px-32">
+    <form className="w-[60%] px-32">
       <h1 className="font-bold text-2xl mb-5">Вход</h1>
 
       <div className="flex flex-col justify-center items-center self-center gap-y-5">
-        <Input label="Номер телефона" placeholder="+79991112233" size="sm" />
+        <Input {...register('phone', { required: true })} label="Номер телефона" placeholder="+79991112233" size="sm" />
         <div className="flex gap-2 items-center ml-auto -mt-3">
           <p className="text-sm text-gray-400">Есть аккаунт? </p>
           <Link className="text-sm" href="register" underline="hover">
             Зарегистрироваться
           </Link>
         </div>
-        <Button color="primary" size="lg">
+        <Button onClick={handleSubmit(onSubmit)} color="primary" size="lg">
           Войти
         </Button>
+        {shownPhoneToCall && (
+          <div className="flex flex-col gap-5 items-center">
+            <Card shadow="sm">
+              <CardHeader>
+                <h1 className="text-xl">Позвоните по данному номеру и вы войдете в систему</h1>
+              </CardHeader>
+              <CardBody className="text-lg font-semibold text-center justify-center text-green-500">
+                {phoneToCall}
+              </CardBody>
+            </Card>
+            <p>Ваш сохраненный номер: {savedPhone}</p>
+            <Button color="primary" variant="solid" onClick={showPhoneToCallHandler}>
+              Войти
+            </Button>
+          </div>
+        )}
       </div>
-    </div>
+    </form>
   );
 }
--- client/components/auth/register/register-form.tsx
+++ client/components/auth/register/register-form.tsx
@@ -1,23 +1,61 @@
 'use client';
 import { Button } from '@nextui-org/button';
-import { DatePicker } from '@nextui-org/date-picker';
 import { Input } from '@nextui-org/input';
 import { Link } from '@nextui-org/link';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+
+import { checkAuth, registerUser } from '@/store/api-actions';
+import { RegisterInput, useAppDispatch, useAppSelector } from '@/types';
+import { IRegisterForm } from '@/types/auth-types';
+import { Card, CardBody, CardHeader } from '@nextui-org/card';
+import { useState } from 'react';
 
 export default function RegisterForm() {
+  const { register, handleSubmit } = useForm<IRegisterForm>();
+  const dispatch = useAppDispatch();
+  const navigation = useRouter();
+  const phoneToCall = useAppSelector((state) => state.DATA_STATE.phoneToCall);
+  const [shownPhoneToCall, setShownPhoneToCall] = useState(false);
+  const [savedPhone, setSavedPhone] = useState<string>('');
+
+  const onSubmit = (data: RegisterInput) => {
+    dispatch(registerUser({ registerUserInput: data })).then((reqData) => {
+      if (reqData.meta.requestStatus === 'fulfilled') {
+        setShownPhoneToCall(true);
+        console.log(phoneToCall);
+        setSavedPhone(data.phone);
+      }
+    });
+  };
+
+  const dataD = {
+    phone: savedPhone,
+  };
+
+  const showPhoneToCallHandler = () => {
+    dispatch(checkAuth({ registerUserInput: dataD })).then((reqData) => {
+      if (reqData.meta.requestStatus === 'fulfilled') {
+        navigation.push('/');
+      } else {
+        // TODO: show error message
+        console.log(reqData);
+      }
+    });
+  };
+
   return (
-    <div className="w-[60%] px-32">
+    <form className="w-[60%] px-32">
       <h1 className="font-bold text-2xl mb-5">Регистрация</h1>
       <div className="flex flex-col justify-center items-center self-center gap-y-5">
         <div className="flex gap-2 w-full">
-          <Input label="Имя" placeholder="Иван" size="sm" />
-          <Input label="Фамилия" placeholder="Иванов" size="sm" />
+          <Input {...register('firstName', { required: true })} label="Имя" placeholder="Иван" size="sm" />
+          <Input {...register('lastName', { required: true })} label="Фамилия" placeholder="Иванов" size="sm" />
         </div>
-        <Input label="Номер телефона" placeholder="+79991112233" size="sm" />
-        <Input label="Email" placeholder="JohnDoe@mail.ru" size="sm" />
-        <div className="w-full max-w-xl flex flex-row gap-4">
+        <Input {...register('phone', { required: true })} label="Номер телефона" placeholder="79991112233" size="sm" />
+        {/* <div className="w-full max-w-xl flex flex-row gap-4">
           <DatePicker showMonthAndYearPickers label="Дата Рождения" />
-        </div>
+        </div> */}
         <div className="flex gap-x-2 items-center ml-auto -mt-3">
           <p className="text-sm text-gray-400">Есть аккаунт? </p>
           <Link className="text-sm" href="login" underline="hover">
@@ -25,10 +63,26 @@ export default function RegisterForm() {
           </Link>
         </div>
 
-        <Button color="primary" size="lg">
+        <Button isDisabled={shownPhoneToCall} color="primary" size="lg" onClick={handleSubmit(onSubmit)}>
           Зарегистрироваться
         </Button>
+        {shownPhoneToCall && (
+          <div className="flex flex-col gap-5 items-center">
+            <Card shadow="sm">
+              <CardHeader>
+                <h1 className="text-xl">Позвоните по данному номеру и вы войдете в систему</h1>
+              </CardHeader>
+              <CardBody className="text-lg font-semibold text-center justify-center text-green-500">
+                {phoneToCall}
+              </CardBody>
+            </Card>
+            <p>Ваш сохраненный номер: {savedPhone}</p>
+            <Button variant="solid" color="primary" onClick={showPhoneToCallHandler}>
+              Войти
+            </Button>
+          </div>
+        )}
       </div>
-    </div>
+    </form>
   );
 }
