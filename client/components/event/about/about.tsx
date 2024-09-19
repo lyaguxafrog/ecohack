507e437a1ec589daf3eb0988ec45e8186ba7550f
Kostya <lukyanovKR@gmail.com>
2024-09-19 04:51:35 +0300

    [~] Фикс textarea

--- client/components/event/about/about.tsx
+++ client/components/event/about/about.tsx
@@ -1,10 +1,13 @@
-import { Textarea } from '@nextui-org/input';
-
 export default function EventAbout() {
   return (
     <div className="mt-10">
       <h1 className="text-2xl font-bold">О событии</h1>
-      <Textarea />
+      <p className="text-medium font-normal">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet velit a arcu semper malesuada. Sed et
+        libero vitae justo malesuada condimentum. Sed non ipsum ac lectus semper bibendum vel vel neque. Donec in ipsum
+        vel neque hendrerit eleifend. Vestibulum euismod, neque vel ullamcorper consectetur, metus nunc ultricies lacus,
+        non sollicitudin eros libero vel ex. Nulla facilisi
+      </p>
     </div>
   );
 }
